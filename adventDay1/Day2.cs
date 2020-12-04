c9bdb416255a1172dc47559134fa72723e2945a0
Sam Greene <lindinopt@gmail.com>
2020-12-04 20:02:12 +0000

    day2 + day2 part 2

--- adventDay1/Day2.cs
+++ adventDay1/Day2.cs
@@ -1,18 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Advent2020
 {
     class Day2
     {
-
-        static string[] readAllLines(string filePath)
+        static public string[] readAllLines(string filePath)
         {
             string[] fileArray = System.IO.File.ReadAllLines(filePath);
 
             return fileArray;
+        }
 
-    }
+        public static int getFileLength(string fileName)
+        {
+            int fileLength = System.IO.File.ReadAllLines(fileName).Length;
+
+            return fileLength;
+        }
+
+        static public int getLineMin(string[] inputArray, int index)
+        {
+            string singleLine = inputArray[index];
+            int occurenceNumMin = Convert.ToInt32(singleLine.Substring(0, singleLine.IndexOf("-")));
+
+            return occurenceNumMin;
+        }
+
+        static public int getLineMax(string[] inputArray, int index)
+        {
+            string singleLine = inputArray[index];
+            int occurenceNumMax = Convert.ToInt32(singleLine.Substring(singleLine.IndexOf("-")+1, singleLine.IndexOf(" ")-2));
+
+            return occurenceNumMax;
+        }
+
+        static public string getLineChar(string[] inputArray, int index)
+        {
+            string singleLine = inputArray[index];
+            string lookupChar = singleLine.Substring(singleLine.IndexOf(" ")+1, singleLine.IndexOf(":")-4);
+
+            if (lookupChar.Contains(":"))
+            {
+                lookupChar = lookupChar.Replace(":", "");
+            }
+
+            lookupChar = lookupChar.Trim();
+
+            return lookupChar;
+        }
+
+        static public string getLinePassword(string[] inputArray, int index)
+        {
+            string singleLine = inputArray[index];
+            int colonIndex = singleLine.IndexOf(":")+2;
+            string password = singleLine.Substring(colonIndex);
+            return password;
+        }
     }
 }
--- adventDay1/Program.cs
+++ adventDay1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Advent2020
 {
@@ -14,9 +15,10 @@ namespace Advent2020
             Console.WriteLine("choose where to go: " + "\n");
             Console.WriteLine("(1) - Day 1");
             Console.WriteLine("(2) - Day 2");
-            Console.WriteLine("\n");
+            Console.WriteLine("(3) - Day 2 Part 2" + "\n");
 
             short choice = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine();
 
             if (choice == 1)
             {
@@ -25,7 +27,14 @@ namespace Advent2020
 
             if (choice == 2)
             {
-                Console.WriteLine("there is no day 2");
+                Console.WriteLine("Beginning:::");
+                startDay2();
+            }
+
+            if (choice == 3)
+            {
+                Console.WriteLine("Beginning:::");
+                startDay2Part2();
             }
             
         }
@@ -45,6 +54,74 @@ namespace Advent2020
             //printLinesInt(convertToIntArray(lines, fileLength));
 
             Console.WriteLine(Day1.getAnswerSum(Day1.convertToIntArray(lines, fileLength), fileLength));
+        }
+
+        public static void startDay2()
+        {
+            int validPasswordsCount = 0;
+
+            string filePath = @"C:\Users\Sam\Downloads\input2.txt";
+            string[] day2Array = Day2.readAllLines(filePath);
+            int fileLength = Day2.getFileLength(filePath);
+            int index = 1;
+            Console.WriteLine("Password ({0}) is ({1}) to ({2}) long and we are looking for the character ({3}) and the password is ({4})", index, Day2.getLineMin(day2Array, index), Day2.getLineMax(day2Array, index), Day2.getLineChar(day2Array, index), Day2.getLinePassword(day2Array, index));
+
+            for (int i = 0; i < day2Array.Length; i++)
+            {
+                string password = Day2.getLinePassword(day2Array, i);
+                int minOccurence = Day2.getLineMin(day2Array, i);
+                int maxOccurence = Day2.getLineMax(day2Array, i);
+                char lookupChar = Convert.ToChar(Day2.getLineChar(day2Array, i));
+
+                int charFreq = password.Count(f => (f == lookupChar));
+                if (charFreq >= minOccurence && charFreq <= maxOccurence)
+                {
+                    validPasswordsCount++;
+                }
+            }
+
+            Console.WriteLine(validPasswordsCount);
+
+            //Console.WriteLine(Day2.getLineMax(day2Array, 1));
+
+        }
+
+        public static void startDay2Part2()
+        {
+            int validPasswordsCount = 0;
+            string filePath = @"C:\Users\Sam\Downloads\input2.txt";
+            string[] day2Array = Day2.readAllLines(filePath);
+
+            for (int i = 0; i < day2Array.Length; i++)
+            {
+                string password = Day2.getLinePassword(day2Array, i);
+                int posOfChar = Day2.getLineMin(day2Array, i);
+                int endPosNotChar = Day2.getLineMax(day2Array, i);
+                char lookupChar = Convert.ToChar(Day2.getLineChar(day2Array, i));
+
+                char[] charArray = password.ToCharArray();
+
+                int charArrayLength = charArray.Length;
+
+                char space = (char)32;
+
+                List<char> tempList = charArray.ToList();
+                tempList.Add(space);
+                charArray = tempList.ToArray();
+
+                if (!(charArray[posOfChar - 1] == lookupChar && charArray[endPosNotChar - 1] == lookupChar))
+                {
+                    if (!(charArray[posOfChar - 1] != lookupChar && charArray[endPosNotChar - 1] != lookupChar))
+                    {
+                        if (charArray[posOfChar - 1] == lookupChar && charArray[endPosNotChar - 1] != lookupChar || charArray[posOfChar - 1] != lookupChar && charArray[endPosNotChar - 1] == lookupChar)
+                        {
+                            validPasswordsCount++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(validPasswordsCount);
         }
     }
 }
